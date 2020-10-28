194c89b99da33d65595cbea9d5bef0b45dea9df2
neychev <neichevxxx@gmail.com>
2020-10-28 16:01:33 +0300

    testing2

--- example_script.py
+++ example_script.py
@@ -4,5 +4,13 @@ import os
 import numpy as np
 from torch import nn
 
+
+def this_is_function(x):
+    """
+    And this is doc
+    """
+    return x / 2
+
+
 print("Hello world!")
 print(2 + 2 / 4)
